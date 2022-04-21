e17c5b62eab9139cd2a0ae136d14e684cfa5d661
guischlindwein <guilherme.matiola@integrejr.com>
2022-04-21 11:36:54 -0300

    page de criação de questões atualizada 11:30

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,22 @@ function cleanPage() {
     principal.innerHTML = "";
 }
 
+
+function insertAnswers(){ //insere as questoes na criação de perguntas 
+    document.querySelector(".other-question").innerHTML+=`
+    <div class="table-question">
+        <input class="questions-answers" type="text" placeholder="Texto da pergunta">
+        <input class="questions-answers" type="text" placeholder="Cor de fundo da pergunta">
+        <h2>Resposta correta</h2>
+        <input class="questions-answers" type="text" placeholder="Resposta correta">
+        <input class="questions-answers" type="text" placeholder="URL de imagem">
+        <h2>Respostas incorretas</h2>
+        <input class="questions-answers" type="text" placeholder="Resposta incorreta 1">
+        <input class="questions-answers" type="text" placeholder="URL de imagem 1">
+        <input class="questions-answers" type="text" placeholder="Resposta incorreta 2">
+        <input class="questions-answers" type="text" placeholder="URL de imagem 2">
+        <input class="questions-answers" type="text" placeholder="Resposta incorreta 3">
+        <input class="questions-answers" type="text" placeholder="URL de imagem 3">
+    </div>
+    `
+}
