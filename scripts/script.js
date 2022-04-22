c69261c63c5226e57bfc42a070445a9865398bdb
guischlindwein <guilherme.matiola@integrejr.com>
2022-04-22 09:31:20 -0300

    inserção de perguntas e niveis dinamicamente ok, está quase sendo postado o quizz criado

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,75 @@
+let listQuizz = [];
+let title;
+let image;
+let questions;
+let levels;
+
+let createdQuizz = {
+
+    title: "Título do quizz",
+    image: "https://t.ctcdn.com.br/5XPASDBUosgmBv5Ptpxcd6eTJso=/512x288/smart/filters:format(webp)/i257652.jpeg",
+    questions: [],
+    levels: []
+};
+let question={
+    title: "Título da pergunta 1",
+    color: "#123456",
+    answers: []
+};
+let answer={
+    text: "Texto da resposta 1",
+    image: "https://t.ctcdn.com.br/5XPASDBUosgmBv5Ptpxcd6eTJso=/512x288/smart/filters:format(webp)/i257652.jpeg",
+    isCorrectAnswer: true
+};
+let level ={
+    title: "Título do nível 1",
+    image: "https://t.ctcdn.com.br/5XPASDBUosgmBv5Ptpxcd6eTJso=/512x288/smart/filters:format(webp)/i257652.jpeg",
+    text: "Descrição do nível 1",
+    minValue: 0
+};
+
+
+
+function homepage(){
+    document.querySelector("main").innerHTML=`
+    <div class="first-pageQuizzes">
+    <button onclick="creatQuizz();"><ion-icon name="add-circle"></ion-icon></button> 
+        <div class="insert-quizz flex-container">
+            <p class="null-quizz">Você ainda não inseriu <br> nenhum quizz :(</p>
+            <button class="create-quizz-button" onclick="createQuizz();">Criar Quizz</button>
+        </div>
+        <div class="textQuizzTitle">
+        <h2> Todos os quizzes</h2>
+        </div>
+
+        
+    </div>    
+    <div class="other-quizzes"></div>
+    `;
+    getQuizzes();
+}
+homepage(); //cria a pagina inicial ou volta pra ela quando preciso
+
+function getQuizzes(){ //faz get na lista de quizzes
+    let promise = axios.get('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes');
+    promise.then(printQuizzes);
+}
+
+function printQuizzes(quizzes){ //mostra a lista de quizzes no html
+    let oQuizzes = document.querySelector(".other-quizzes"); // oQuizzes => otherQuizzes
+    listaQuizz = quizzes.data;
+    console.log(quizzes.data);
+    for(i = 0; i < listaQuizz.length; i++){     // ADICIONAR OS QUIZZES DO SERVER
+        oQuizzes.innerHTML += ` 
+        <button onclick="showQuizz(${i})" class="quizzBox"> 
+        <img src="${listaQuizz[i].image}" alt="thumb"> 
+         
+        <h4 class="QuizzTitle white"> ${listaQuizz[i].title} </h4>
+        </button>`
+    }
+}
+
+
 function createQuizz() {
   //  cleanPage();
    // displayFirstCreationPage();
@@ -6,142 +78,190 @@ function createQuizz() {
     
     <strong>Começando pelo começo</strong>
         <div class="quizz-creation flex-container">
-            <input type="text" placeholder="Título do seu quizz">
-            <input type="text" placeholder="URL da imagem do seu quizz">
-            <input type="text" placeholder="Quantidade de perguntas do quizz">
-            <input type="text" placeholder="Quantidade de níveis do quizz">
+            <input id="i1" type="text" minlength="20" maxlength="65" required placeholder="Título do seu quizz">
+            <input id="i2" type="url" required placeholder="URL da imagem do seu quizz">
+            <input id="i3" placeholder="Quantidade de perguntas do quizz" type="number" min="3" required>
+            <input id="i4" placeholder="Quantidade de níveis do quizz" type="number" min="2" required>
         </div>
-        <button class="create-question-button" onclick="questionsCreate();">Prosseguir pra criar perguntas</button>
+        <button class="create-question-button" onclick="readINFOSQuizz();">Prosseguir pra criar perguntas</button>
     `
+                                                      //questionsCreate();
+}
 
+function readINFOSQuizz(){
+    title=document.getElementById("i1").value;
+    image=document.getElementById("i2").value;
+    questions=document.getElementById("i3").value;
+    levels=document.getElementById("i4").value;    
+
+    if(title.length<20 || title.length>65){
+        alert(`escolha um título entre 20 e 65 caracteres. O seu está com ${title.length}`);
+        return;
+    }
+    if(!isValidHttpUrl(image)){
+        alert('Insira uma url de imagem')
+        return;
+    }
+    if(questions<3){
+        alert('São necessárias 3 perguntas no minimo para o quizz');
+        return;
+    }
+    if(levels<2){
+        alert('São necessárias 2 níveis no minimo para o quizz');
+        return;
+    }
+
+    console.log("title: "+title);
+    createdQuizz.title=title;
+
+    console.log("createdquizz.title: "+createdQuizz.title);
+    createdQuizz.image=image;
+    questionsCreate();
+}
+function isValidHttpUrl(string) { //verifica se a string é url
+    let url;
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+    return true;
 }
 
 function questionsCreate(){
     document.querySelector("main").innerHTML=`
     <div class="quizz-questions">
             <div class="create-questions-title"><strong>Crie suas perguntas</strong></div> 
-            <div class="question-box">
+     </div>
+    `;
 
-                <div class="first-question">
-                    <h2>Pergunta ${1}</h2>
-
-                    <div class="table-question">
-                        <input class="questions-answers" type="text" placeholder="Texto da pergunta">
-                        <input class="questions-answers" type="text" placeholder="Cor de fundo da pergunta">
-                        <h2>Resposta correta</h2>
-                        <input class="questions-answers" type="text" placeholder="Resposta correta">
-                        <input class="questions-answers" type="text" placeholder="URL de imagem">
-                        <h2>Respostas incorretas</h2>
-                        <input class="questions-answers" type="text" placeholder="Resposta incorreta 1">
-                        <input class="questions-answers" type="text" placeholder="URL de imagem 1">
-                        <input class="questions-answers" type="text" placeholder="Resposta incorreta 2">
-                        <input class="questions-answers" type="text" placeholder="URL de imagem 2">
-                        <input class="questions-answers" type="text" placeholder="Resposta incorreta 3">
-                        <input class="questions-answers" type="text" placeholder="URL de imagem 3">
-                    </div>
-                    
-                </div>
-
-                <div class="other-question">
-                    <div class="next-question">
-                        <h2>Pergunta ${2}</h2>
-                        <img class="vector-image" src="media/Vector.png" onclick="insertAnswers()">
-                    </div>
-                </div>
-
-                <div class="first-question">
-                    <div class="next-question">
-                        <h2>Pergunta ${3}</h2>
-                        <img class="vector-image" src="media/Vector.png" onclick="insertAnswers()">
-                    </div>
-                </div>
-
-                <button class="create-levels-button" onclick="levelsCreate();">Prosseguir pra criar níveis</button>
-
-                
-            </div>
-        </div>
+    for(let i=0;i<questions;i++){
+        document.querySelector("main").innerHTML+=
+        `
+        <div class="quizz-questions">
+               <div class="question-box">
+   
+                   <div class="first-question">
+                       <h2>Pergunta ${i+1}</h2>
+   
+                       <div class="table-question">
+                           <input id="a${i+1}1" class="questions-answers" type="text" placeholder="Texto da pergunta">
+                           <input id="a${i+1}2" class="questions-answers" type="text" placeholder="Cor de fundo da pergunta">
+                           <h2>Resposta correta</h2>
+                           <input id="a${i+1}3" class="questions-answers" type="text" placeholder="Resposta correta">
+                           <input id="a${i+1}4" class="questions-answers" type="url" placeholder="URL de imagem">
+                           <h2>Respostas incorretas</h2>
+                           <input id="a${i+1}5" class="questions-answers" type="text" placeholder="Resposta incorreta 1">
+                           <input id="a${i+1}6" class="questions-answers" type="url" placeholder="URL de imagem 1">
+                           <input id="a${i+1}7" class="questions-answers" type="text" placeholder="Resposta incorreta 2">
+                           <input id="a${i+1}8" class="questions-answers" type="url" placeholder="URL de imagem 2">
+                           <input id="a${i+1}9" class="questions-answers" type="text" placeholder="Resposta incorreta 3">
+                           <input id="a${i+1}10" class="questions-answers" type="url" placeholder="URL de imagem 3">
+                       </div>
+                       
+                   </div>
+   
+   
+                  
+   
+                   
+               </div>
+           </div>
+       
+       `
+    }
     
-    `
-
+    document.querySelector("main").innerHTML+=`
+    <button class="create-levels-button" onclick="readINFOQuizzPg2();">Prosseguir pra criar níveis</button>                                
+    `;                                          //levelsCreate();
 }
+function readINFOQuizzPg2() {
+    for(let i =0;i<questions;i++){
+        question.title=document.getElementById(`a${i+1}1`).value;
+        question.color=document.getElementById(`a${i+1}2`).value;
+        
+        answer.text=document.getElementById(`a${i+1}3`).value;
+        answer.image=document.getElementById(`a${i+1}4`).value;
+        answer.isCorrectAnswer=true;
+        question.answers[0]=answer;
+        answer.text=document.getElementById(`a${i+1}5`).value;
+        answer.image=document.getElementById(`a${i+1}6`).value;
+        answer.isCorrectAnswer=false;
+        question.answers[1]=answer;
+        //QUANTAS RESPOSTAS???????????????????!!!!!!!!!!!
+        answer.text=document.getElementById(`a${i+1}7`).value;
+        answer.image=document.getElementById(`a${i+1}8`).value;
+        answer.isCorrectAnswer=false;
+        question.answers[2]=answer;
+        answer.text=document.getElementById(`a${i+1}9`).value;
+        answer.image=document.getElementById(`a${i+1}10`).value;
+        answer.isCorrectAnswer=false;
+        question.answers[3]=answer;
+
+        createdQuizz.questions[i]=question;
+    }
+    levelsCreate();
+}
+
 function levelsCreate(){
     document.querySelector("main").innerHTML=`
     
     <div class="quizz-levels">
             <div class="create-questions-title"><strong>Agora, decida os níveis</strong></div> 
-
+    </div>
+    `
+    for(let i=0;i<levels;i++){
+        document.querySelector("main").innerHTML+=
+        `
             <div class="question-box">
 
                 <div class="first-question">
-                    <h2>Nível ${1}</h2>
+                    <h2>Nível ${i+1}</h2>
 
                     <div class="table-question">
-                        <input class="questions-answers" type="text" placeholder="Título do nível">
-                        <input class="questions-answers" type="text" placeholder="% de acerto mínima">
-                        <input class="questions-answers" type="text" placeholder="URL de imagem do nível">
-                        <input class="questions-answers" type="text" placeholder="Descrição do nível">
+                        <input id="a${i+1}1" class="questions-answers" type="text" placeholder="Título do nível">
+                        <input id="a${i+1}2" class="questions-answers" type="number" placeholder="% de acerto mínima">
+                        <input id="a${i+1}3" class="questions-answers" type="url" placeholder="URL de imagem do nível">
+                        <input id="a${i+1}4" class="questions-answers" type="text" placeholder="Descrição do nível">
                         
                     </div>
                     
                 </div>
 
-                <div class="other-question">
-                    <div class="next-question">
-                        <h2>Nível ${2}</h2>
-                        <img class="vector-image" src="media/Vector.png" onclick="insertAnswers()">
-                    </div>
-                </div>
+                
 
-                <div class="first-question">
-                    <div class="next-question">
-                        <h2>Nível ${3}</h2>
-                        <img class="vector-image" src="media/Vector.png" onclick="insertAnswers()">
-                    </div>
-                </div>
-
-                <button class="create-levels-button">Finizalizar Quizz</button>
+                
 
                 
             </div>
+    `
+    }
 
-
-        </div>
     
-    `
+    document.querySelector("main").innerHTML+=`
+        <button class="create-levels-button" onclick="readINFOQuizzPg3();">Finalizar Quizz</button>
+    `;
 }
+function readINFOQuizzPg3() {
+    for(let i =0;i<levels;i++){
+        level.title=document.getElementById(`a${i+1}1`).value;
+        level.minValue=document.getElementById(`a${i+1}2`).value;
+        level.image=document.getElementById(`a${i+1}3`).value;
+        level.text=document.getElementById(`a${i+1}4`).value;
+        createdQuizz.levels[i]=level;
+    }
+    console.log(createdQuizz);
+    let promise=axios.post('https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes',createdQuizz);
+    promise.then(postedQuizz());
+}
+function postedQuizz(){
+    alert("sucess");
+    homepage();
+}
+//Codigo executado ao iniciar
+homepage();
 
-function cleanPage() {
-    const principal = document.querySelector("main");
-    principal.innerHTML = "";
-}
-
-function homepage(){
-    document.querySelector("main").innerHTML=`
-    <button onclick="creatQuizz();"><ion-icon name="add-circle"></ion-icon></button> 
-        <div class="insert-quizz flex-container">
-            <p class="null-quizz">Você ainda não inseriu <br> nenhum quizz :(</p>
-            <button class="create-quizz-button" onclick="createQuizz();">Criar Quizz</button>
-        </div>
-    `
-}
-homepage(); //cria a pagina inicial ou volta pra ela quando preciso
-
-function insertAnswers(){ //insere as questoes na criação de perguntas 
-    document.querySelector(".other-question").innerHTML+=`
-    <div class="table-question">
-        <input class="questions-answers" type="text" placeholder="Texto da pergunta">
-        <input class="questions-answers" type="text" placeholder="Cor de fundo da pergunta">
-        <h2>Resposta correta</h2>
-        <input class="questions-answers" type="text" placeholder="Resposta correta">
-        <input class="questions-answers" type="text" placeholder="URL de imagem">
-        <h2>Respostas incorretas</h2>
-        <input class="questions-answers" type="text" placeholder="Resposta incorreta 1">
-        <input class="questions-answers" type="text" placeholder="URL de imagem 1">
-        <input class="questions-answers" type="text" placeholder="Resposta incorreta 2">
-        <input class="questions-answers" type="text" placeholder="URL de imagem 2">
-        <input class="questions-answers" type="text" placeholder="Resposta incorreta 3">
-        <input class="questions-answers" type="text" placeholder="URL de imagem 3">
-    </div>
-    `
-}
+// function cleanPage() {
+//     const principal = document.querySelector("main");
+//     principal.innerHTML = "";
+// }
